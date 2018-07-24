5b38cdc8efe4c23e4cf5bbc9cab49f94046f5710
SukkaW <isukkaw@gmail.com>
2018-07-24 12:59:08 +0800

    refactor(ci): update a lot - use npm script instead of cli - fix git deployer

--- test/build.sh
+++ test/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-hexo clean
-hexo g
+npm run clean
+npm run generate
--- test/deploy.sh
+++ test/deploy.sh
@@ -9,9 +9,9 @@ git config --global user.email "isukkaw@gmail.com"
 git config --global user.name "SukkaW"
 # Clone remote
 git remote add origin https://${GitHubKEY}@github.com/theme-suka/demo.git
-git pull
+git pull origin master
 rm -rf ./*
 cp -rf ../public/* ./
 git add --all .
 git commit -m "Travis Auto Tester for hexo-theme-suka"
-git push --quiet --force https://${GitHubKEY}@github.com/theme-suka/demo.git
+git push --quiet --force origin HEAD:master
