9749743e3389ee0f3ef8ae0049176e79f9414667
SukkaW <isukkaw@gmail.com>
2018-12-28 01:14:48 +0800

    feat/refactor(qrcode): use es6 and simplify url

--- scripts/qrcode/index.js
+++ scripts/qrcode/index.js
@@ -1,15 +1,20 @@
 const qrImage = require('qr-image')
 
-hexo.extend.helper.register('qrcode', function(url, option){
-    option = Object.assign({
-        size: 6,
-        margin: 0
-    }, option || {})
+hexo.extend.helper.register('qrcode', (url, option) => {
+    option = Object.assign(
+        {
+            size: 6,
+            margin: 0
+        },
+        option || {}
+    )
+
+    url = url.replace('index.html', '');
 
     const buffer = qrImage.imageSync(url, {
         type: 'png',
         size: option.size,
         margin: option.margin
     })
-    return 'data:image/png;base64,' + buffer.toString('base64')
+    return `data:image/png;base64,${buffer.toString('base64')}`
 })
