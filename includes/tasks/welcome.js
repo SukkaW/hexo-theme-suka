9c30d55ce5109fa9a0a7b62b031ca399f3ed0393
SukkaW <isukkaw@gmail.com>
2019-02-01 23:20:26 +0800

    feat(includes/welcome): print welcome message

---
+++ includes/tasks/welcome.js
@@ -0,0 +1,7 @@
+const logger = require('hexo-log')();
+
+logger.info(`
+----------------------------------------
+Suka Theme (https://theme-suka.skk.moe)
+----------------------------------------
+`);
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+// Welcome Message
+require('../includes/tasks/welcome');
+
+
 // Debug helper
 hexo.extend.helper.register('console', function () {
     console.log(arguments)
