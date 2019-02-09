87fd40d459b05bb3ea6605190c4421fd5172efbb
SukkaW <isukkaw@gmail.com>
2019-02-09 18:50:34 +0800

    refactor(helper/title): bring up new title helper

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,9 @@ require('../includes/tasks/welcome');
 // Check required dependencies
 require('../includes/tasks/check_deps');
 
+// Tiele Helper
+require('../includes/helpers/title')(hexo);
+
 // Debug helper
 hexo.extend.helper.register('console', function () {
     console.log(arguments)
