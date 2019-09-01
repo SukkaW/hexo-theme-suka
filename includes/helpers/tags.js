610a81a7b90ba3767501212a75d302e6c7fa5eb7
SukkaW <isukkaw@gmail.com>
2019-09-01 22:06:11 +0800

    feat: support load async css

--- includes/helpers/tags.js
+++ includes/helpers/tags.js
@@ -1,3 +1,5 @@
 module.exports = function (hexo) {
     hexo.extend.helper.register('_meta_generator', () => `<meta name="generator" content="Hexo ${hexo.version}">`);
+
+    hexo.extend.helper.register('css_async', (url) => `<link rel="preload" href="${url}" as="style" onload="this.onload=null;this.rel='stylesheet'"><noscript><link rel="stylesheet" href="${url}"></noscript>`);
 };
