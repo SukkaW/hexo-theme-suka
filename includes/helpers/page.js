fa5b514a2abad63b6e9a1b6eb05347964fd4b6ac
SukkaW <isukkaw@gmail.com>
2019-09-01 16:30:12 +0800

    refactor(favicon): bring up a favicon helper

--- includes/helpers/page.js
+++ includes/helpers/page.js
@@ -3,7 +3,34 @@
   * @example
   *     <%- page_title(page) %>
   */
+
 module.exports = function (hexo) {
+    const { htmlTag, stripHTML } = require('hexo-util');
+
+    hexo.extend.helper.register('favicon', function (options) {
+        const url_for = hexo.extend.helper.get('url_for').bind(this);
+
+        let html = '';
+
+        if (options.ico) {
+            html += htmlTag('link', { rel: 'icon', type: 'image/ico', href: url_for(options.ico) });
+        }
+        if (options.apple_touch_icon) {
+            html += htmlTag('link', { rel: 'apple-touch-icon', sizes: '180x180', href: url_for(options.apple_touch_icon) });
+        }
+        if (options.large) {
+            html += htmlTag('link', { rel: 'icon', typt: 'image/png', sizes: '192x192', href: url_for(options.large) });
+        }
+        if (options.medium) {
+            html += htmlTag('link', { rel: 'icon', typt: 'image/png', sizes: '32x32', href: url_for(options.medium) });
+        }
+        if (options.small) {
+            html += htmlTag('link', { rel: 'icon', typt: 'image/png', sizes: '16x16', href: url_for(options.small) });
+        }
+
+        return html;
+    });
+
     hexo.extend.helper.register('page_title', function (page = null) {
         page = (page === null) ? this.page : page;
 
@@ -32,7 +59,7 @@ module.exports = function (hexo) {
 
         let description = hexo.config.description || page.description || page.excerpt || page.content;
 
-        description = this.strip_html(description).trim() // Remove prefixing/trailing spaces
+        description = stripHTML(description).trim() // Remove prefixing/trailing spaces
             .replace(/^s*/, '').replace(/s*$/, '')
             .substring(0, 200)
             .replace(/</g, '&lt;')
